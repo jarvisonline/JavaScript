fbec7fb6fd441ae71f64fd2a9c804acf1d286284
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-02-27 13:12:19 +0530

    String methods in js✅✅

---
+++ JS3/js/main.js
@@ -0,0 +1,12 @@
+const myVariable = "Mathematics";
+
+console.log(myVariable.charAt(5));
+console.log(myVariable.indexOf("m"));
+console.log(myVariable.lastIndexOf("s"));
+console.log(myVariable.slice(2, 5));
+console.log(myVariable.toUpperCase(5));
+console.log(myVariable.includes(5));
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""))
+console.log("Akhand,Single,Hu".split(","));
+

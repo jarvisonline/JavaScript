4f5592893160e4a77c1f60b77a94beaebe321ca4
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-03-04 20:50:28 +0530

    Arrays=[ ] 💪💪

---
+++ JS15/js/main.js
@@ -0,0 +1,70 @@
+//Arrays
+const myArrayA = ["A", "B", "C"]
+const myArrayB=["D", "E", "F"];
+
+// add elements to an array
+/* myArray[0] = "Hello";
+myArray[1] = 9;
+myArray[2] = false; */
+
+/* //refer to an array
+console.log(myArray);
+//accessing array element by index
+
+//length property
+console.log(myArray.length);
+
+//last element in an array
+console.log(myArray[myArray.length-1]);
+
+console.log(myArray[1]); */
+
+/* myArray.push("school");
+console.log(myArray); */
+
+/* const lastItem= myArray.pop();
+
+console.log(lastItem); */
+
+/* myArray.unshift(42); */
+//const newLength = myArray.shift(42);
+//unshift () method adds new items to the start of an array and shifts other existing items to higher indexes
+/* console.log(myArray); */
+//console.log(newLength);
+
+/* const firstItem = myArray.shift();
+console.log(firstItem);
+console.log(myArray);
+console.log(myArray[1]); */
+
+/* delete myArray[1];
+console.log(myArray);
+console.log(myArray[1]) */
+
+/* myArray.splice(1,0,42); //removes one item at a specific position
+console.log(myArray);
+console.log(myArray[1]); */
+
+/* const newArray=myArray.slice(2,5);
+console.log(newArray); */
+
+/* const newArray = myArray.slice(2, 5);
+console.log(newArray); */
+
+
+/* myArray.reverse();
+console.log(myArray); */
+
+/* const newString=myArray.join();
+
+const newArray=newString.split(",");
+
+console.log(newArray); */
+
+/* const newArray=myArrayB.concat(myArrayA);
+
+console.log(newArray); */
+
+const newArray=[myArrayB,myArrayA];
+console.log(newArray);
+
